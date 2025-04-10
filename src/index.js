88ea1c1edcda9086b75297186ec29de656a7601f
Diljot <diljots99@gmail.com>
2025-04-10 10:29:13 -0700

    Update package version to 0.0.3 and add exports for guillotine and packer functionalities in index.js

---
+++ src/index.js
@@ -0,0 +1,59 @@
+// guillotine.js
+import {
+    GuillotineBssfSas, GuillotineBssfLas,
+    GuillotineBssfSlas, GuillotineBssfLlas,
+    GuillotineBssfMaxas, GuillotineBssfMinas,
+    GuillotineBlsfSas, GuillotineBlsfLas,
+    GuillotineBlsfSlas, GuillotineBlsfLlas,
+    GuillotineBlsfMaxas, GuillotineBlsfMinas,
+    GuillotineBafSas, GuillotineBafLas,
+    GuillotineBafSlas, GuillotineBafLlas,
+    GuillotineBafMaxas, GuillotineBafMinas
+  } from './rectpack/guillotine.js';
+
+
+
+  // packer.js - constants
+  import {
+    SORT_AREA, SORT_PERI, SORT_DIFF, SORT_SSIDE,
+    SORT_LSIDE, SORT_RATIO, SORT_NONE
+  } from './rectpack/packer.js';
+
+  // packer.js - classes and functions
+  import {
+    PackerBNF, PackerBFF, PackerBBF, PackerOnlineBNF,
+    PackerOnlineBFF, PackerOnlineBBF, PackerGlobal,
+    newPacker, PackingMode, PackingBin, float2dec
+  } from './rectpack/packer.js';
+
+  // guillotine.js
+export  {
+  GuillotineBssfSas, GuillotineBssfLas,
+  GuillotineBssfSlas, GuillotineBssfLlas,
+  GuillotineBssfMaxas, GuillotineBssfMinas,
+  GuillotineBlsfSas, GuillotineBlsfLas,
+  GuillotineBlsfSlas, GuillotineBlsfLlas,
+  GuillotineBlsfMaxas, GuillotineBlsfMinas,
+  GuillotineBafSas, GuillotineBafLas,
+  GuillotineBafSlas, GuillotineBafLlas,
+  GuillotineBafMaxas, GuillotineBafMinas
+} 
+
+
+
+
+
+// packer.js - constants
+export {
+  SORT_AREA, SORT_PERI, SORT_DIFF, SORT_SSIDE,
+  SORT_LSIDE, SORT_RATIO, SORT_NONE
+} 
+
+// packer.js - classes and functions
+export {
+  PackerBNF, PackerBFF, PackerBBF, PackerOnlineBNF,
+  PackerOnlineBFF, PackerOnlineBBF, PackerGlobal,
+  newPacker, PackingMode, PackingBin, float2dec
+} 
+
+ 
